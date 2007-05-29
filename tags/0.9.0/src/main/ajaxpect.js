84583018f40c32b790f67d1d6973407a13d29f52
roman.porotnikov <roman.porotnikov@164579ee-a630-0410-9deb-c19c3391206c>
2007-05-29 13:36:31 +0000

    Version 0.9.0

---
+++ tags/0.9.0/src/main/ajaxpect.js
@@ -0,0 +1,58 @@
+/*
+ * Ajaxpect 0.9.0
+ * http://code.google.com/p/ajaxpect
+ */
+
+var Ajaxpect = {
+
+  addBefore: function(obj, filter, before) {
+    var link = function(orig) {
+      return function() {
+        return orig.apply(this, before(arguments, orig, this));
+      }
+    }
+    this._process(obj, filter, link);
+  },
+
+  addAfter: function(obj, filter, after) {
+    var link = function(orig) {
+      return function() {
+        return after(orig.apply(this, arguments), arguments, orig, this);
+      }
+    }
+    this._process(obj, filter, link);
+  },
+
+  addAround: function(obj, filter, around) {
+    var link = function(orig) {
+      return function() {
+        return around(arguments, orig, this);
+      }
+    }
+    this._process(obj, filter, link);
+  },
+  
+  _process: function(obj, filter, link) {
+    var check;
+    if (filter.exec) {
+      check = function(str) { return filter.exec(str) }
+    } else if (filter.call) {
+      check = function(str) { return filter.call(this, str) }
+    }
+    if (check) {
+      for (var member in obj) {
+        if (check(member)) {
+          this._attach(obj, member, link);
+        }
+      }
+    } else {
+      this._attach(obj, filter, link);
+    }
+  },
+
+  _attach: function(obj, member, link) {
+    var orig = obj[member];
+    obj[member] = link(orig);
+  }
+  
+}
